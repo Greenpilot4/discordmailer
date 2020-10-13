2cc854a7bbbb602a9224805af6670922b005b547
Greenpilot4 <41490507+Greenpilot4@users.noreply.github.com>
2020-10-13 15:29:18 -0700

    Add files via upload

---
+++ commands/sendmailcommand.js
@@ -0,0 +1,75 @@
+const fs = require("fs");
+const request = require(`request`);
+const Discord = require("discord.js");
+const { host, username, password } = require("./config.json");
+
+const nodemailer = require("nodemailer");
+
+module.exports = {
+    name: 'mail',
+    description: 'Send a spoofed email with discord!',
+	execute(message, args) {
+	    message.reply("Please Enter The Recipients Address");
+        let filter = (m) => !m.author.bot;
+        const collector = new Discord.MessageCollector(
+        message.channel,
+        (m) => m.author.id === message.author.id,
+        { time: 1000000 }
+        );
+
+        var counter = 0;
+
+        collector.on("collect", (message) => {
+            if (message.content && counter == 0) {
+                receiver = message.content;
+                console.log(receiver);
+                message.reply("Recipient Set Please Provide A Sender Address");
+                counter++;
+            } else if (message.content && counter == 1) {
+                ssender = message.content;
+                console.log(message.content);
+                message.reply("Sender Set Please Provide A Subject");
+                counter++;
+            } else if (message.content && counter == 2) {
+                message.reply("Sender Name Set Please Provide A Subject");
+                nsender = message.content;
+                console.log(message.content);
+                counter++;
+            }else if (message.content && counter == 3) {
+                message.reply("Subject Set Please Provide A Message");
+                subject = message.content;
+                console.log(message.content);
+                counter++;
+            } else if (message.content && counter == 4) {
+                    mcontent = message.content;
+                    console.log(mcontent);
+    
+                    let transporter = nodemailer.createTransport({
+                        host: host,
+                        port: 587,
+                        secure: false,
+                        auth: {
+                        user: username,
+                        pass: password,
+                        },
+                        tls: {rejectUnauthorized: false},
+                        debug:true
+                        });
+                        let info = transporter.sendMail({
+                        from: {
+                            name: nsender,
+                            address: ssender,
+                        },
+                        to: receiver,
+                        subject: subject,
+                        html: mcontent,
+                        });
+    
+                    message.reply("Email Sent!");
+                    console.log("Sent Email With Nodemail");
+                    collector.stop();
+                    console.log("Collecter Ended");
+                }
+        });
+	},
+}
---
+++ index.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const Discord = require("discord.js");
+const { prefix, token } = require("./config.json");
+
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.on("message", async (message) => {
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const commandName = args.shift().toLowerCase();
+  const command = client.commands.get(commandName);
+
+  if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+
+
+  try {
+		if(commandName == "poop" || commandName == "devin") {
+			command.execute(message, client); //ignore this for now not needed
+		} else {
+			command.execute(message);
+		}
+	} catch (error) {
+		console.error(error);
+		message.reply('Something Went Wrong!');
+	}
+});
+
+client.login(token);
