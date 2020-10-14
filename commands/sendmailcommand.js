7f2a42e7e5847b52d5f2aac44f3b6e3ffeba9a81
9qq-qq-qq9 <46979929+9qq-qq-qq9@users.noreply.github.com>
2020-10-14 14:14:38 -0700

    finished up html file upload support and also added a date to the logging

--- commands/sendmailcommand.js
+++ commands/sendmailcommand.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 const request = require(`request`);
 const Discord = require("discord.js");
+const http = require('https');
 const { host, port, username, password, logging, logoutput } = require("../config.json");
-
+const currentTime = getDateTime();
 const nodemailer = require("nodemailer");
 
 function log(message) {
@@ -12,12 +13,41 @@ function log(message) {
    }
 }
 
+function getDateTime() {
+        var now     = new Date(); 
+        var year    = now.getFullYear();
+        var month   = now.getMonth()+1; 
+        var day     = now.getDate();
+        var hour    = now.getHours();
+        var minute  = now.getMinutes();
+        var second  = now.getSeconds(); 
+        if(month.toString().length == 1) {
+             month = '0'+month;
+        }
+        if(day.toString().length == 1) {
+             day = '0'+day;
+        }   
+        if(hour.toString().length == 1) {
+             hour = '0'+hour;
+        }
+        if(minute.toString().length == 1) {
+             minute = '0'+minute;
+        }
+        if(second.toString().length == 1) {
+             second = '0'+second;
+        }   
+        var dateTime = year+'/'+month+'/'+day+' '+hour+':'+minute+':'+second;   
+         return dateTime;
+}
+
+
+
 module.exports = {
     name: 'mail',
     description: 'Send a spoofed email with discord!',
 	execute(message, args) {
 	    message.reply("Please Enter The Recipients Address");
-        log(message.author.tag + " has ran the mail command");
+        log(currentTime + " " + message.author.tag +" has ran the mail command");
         let filter = (m) => !m.author.bot;
         const collector = new Discord.MessageCollector(
         message.channel,
@@ -30,27 +60,27 @@ module.exports = {
         collector.on("collect", (message) => {
             if (message.content && counter == 0) {
                 receiver = message.content;
-                log(message.author.tag + " - Mail Recipient Address: " + message.content);
+                log(currentTime + " " + message.author.tag +" - Mail Recipient Address: " + message.content);
                 message.reply("Recipient Set Please Provide A Sender Address");
                 counter++;
             } else if (message.content && counter == 1) {
                 ssender = message.content;
-                log(message.author.tag + " - Mail Sender Address: " + message.content);
+                log(currentTime + " " + message.author.tag +" - Mail Sender Address: " + message.content);
                 message.reply("Sender Set Please Provide A Send Name");
                 counter++;
             } else if (message.content && counter == 2) {
                 message.reply("Sender Name Set Please Provide A Subject");
                 nsender = message.content;
-                log(message.author.tag + " - Mail Sender Name: " + message.content);
+                log(currentTime + " " + message.author.tag +" - Mail Sender Name: " + message.content);
                 counter++;
             } else if (message.content && counter == 3) {
                 message.reply("Subject Set Please Provide A Message");
                 subject = message.content;
-                log(message.author.tag + " - Mail Subject: " + message.content);
+                log(currentTime + " " + message.author.tag +" - Mail Subject: " + message.content);
                 counter++;
             } else if (message.content && counter == 4) {
                 mcontent = message.content;
-                log(message.author.tag + " - Mail Message: " + mcontent);
+                log(currentTime + " " + message.author.tag +" - Mail Message: " + mcontent);
 
                 let transporter = nodemailer.createTransport({
                     host: host,
@@ -79,15 +109,18 @@ module.exports = {
                 collector.stop();
                 log("Collecter Ended");
             } else if (message.attachments.first()) {
-                let request = require(`request`);
-                let fs = require(`fs`);
-                function download(url){
-                    request.get(url)
-                        .on('error', console.error)
-                        .pipe(fs.createWriteStream('./html_content/message.html'));
-                }
-                download(message.attachments.first().url);
-                log(message.author.tag + " - Mail Message: " + 'message.html');
+				var request = http.get(message.attachments.first().url, function(response) {
+    				if (response.statusCode === 200) {
+        				var file = fs.createWriteStream("./html_content/" + message.author.tag  + ".html");
+        				response.pipe(file);
+    				}
+    				// Add timeout.
+    				request.setTimeout(12000, function () {
+        				request.abort();
+    				});
+				});
+
+                log(currentTime + " " + message.author.tag +" - Mail Message: " + "html_content/" + message.author.tag +".html");
 
                 let transporter = nodemailer.createTransport({
                     host: host,
@@ -107,8 +140,8 @@ module.exports = {
                     },
                     to: receiver,
                     subject: subject,
-                    text: ('filehere'),
-                    html: ('filehere'),
+                    text: fs.readFileSync("./html_content/" + message.author.tag  + ".html"),
+                    html: fs.readFileSync("./html_content/" + message.author.tag  + ".html"),
                     });
 
                 message.reply("Email Sent!");
