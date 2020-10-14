934a78233de89c761e404c3f7f80c72a38a1bcb7
9qq-qq-qq9 <46979929+9qq-qq-qq9@users.noreply.github.com>
2020-10-14 10:44:11 -0700

    added a logging system for mail sending

--- commands/sendmailcommand.js
+++ commands/sendmailcommand.js
@@ -5,11 +5,17 @@ const { host, port, username, password } = require("../config.json");
 
 const nodemailer = require("nodemailer");
 
+function log(message) {
+   console.log(message);
+   fs.appendFileSync("log.txt", message + "\n");
+}
+
 module.exports = {
     name: 'mail',
     description: 'Send a spoofed email with discord!',
 	execute(message, args) {
 	    message.reply("Please Enter The Recipients Address");
+        log(message.author.tag + " has ran the mail command");
         let filter = (m) => !m.author.bot;
         const collector = new Discord.MessageCollector(
         message.channel,
@@ -22,27 +28,27 @@ module.exports = {
         collector.on("collect", (message) => {
             if (message.content && counter == 0) {
                 receiver = message.content;
-                console.log(receiver);
+                log(message.author.tag + " - Mail Recipient Address - " + message.content);
                 message.reply("Recipient Set Please Provide A Sender Address");
                 counter++;
             } else if (message.content && counter == 1) {
                 ssender = message.content;
-                console.log(message.content);
+                log(message.author.tag + " - Mail Sender Address - " + message.content);
                 message.reply("Sender Set Please Provide A Send Name");
                 counter++;
             } else if (message.content && counter == 2) {
                 message.reply("Sender Name Set Please Provide A Subject");
                 nsender = message.content;
-                console.log(message.content);
+                log(message.author.tag + " - Mail Sender Name - " + message.content);
                 counter++;
             }else if (message.content && counter == 3) {
                 message.reply("Subject Set Please Provide A Message");
                 subject = message.content;
-                console.log(message.content);
+                log(message.author.tag + " - Mail Subject - " + message.content);
                 counter++;
             } else if (message.content && counter == 4) {
                     mcontent = message.content;
-                    console.log(mcontent);
+                    log(message.author.tag + " - Mail Message - " + mcontent);
     
                     let transporter = nodemailer.createTransport({
                         host: host,
@@ -66,9 +72,9 @@ module.exports = {
                         });
     
                     message.reply("Email Sent!");
-                    console.log("Sent Email With Nodemail");
+                    log("Sent Email With Nodemail");
                     collector.stop();
-                    console.log("Collecter Ended");
+                    log("Collecter Ended");
                 }
         });
 	},
