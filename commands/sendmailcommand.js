80699358dd6832f0f71cfe7680c8c2a6e5bd693a
ギャビンコラード <dev.greenpilot@gmail.com>
2020-11-08 04:11:16 +0000

    logging and structure changes

--- commands/sendmailcommand.js
+++ commands/sendmailcommand.js
@@ -3,104 +3,24 @@ const http = require('https');
 const path = require("path");
 const request = require(`request`);
 const Discord = require("discord.js");
-const nodemailer = require("nodemailer");
-const { host, port, username, password, logging, logoutput } = require("../config.json");
+
+const currentTime = require("../events/time.js");
+const log = require("../events/log.js");
+const sendmail = require("../events/sendmail.js");
 
 mattachment = null
-
-function log(message) {
-   console.log(message);
-   if (logging == "true"){
-    fs.appendFileSync(logoutput, message + "\n");
-   }
-}
-
-process.on( "SIGINT", function() {
-    console.log("Goodbye!")
-    log("-----------------------------------------------------------------------------------------");
-    process.exit();
-});
-
-const currentTime = getDateTime();
-function getDateTime() {
-        var now     = new Date(); 
-        var year    = now.getFullYear();
-        var month   = now.getMonth()+1; 
-        var day     = now.getDate();
-        var hour    = now.getHours();
-        var minute  = now.getMinutes();
-        var second  = now.getSeconds(); 
-        if(month.toString().length == 1) {
-             month = '0'+month;
-        }
-        if(day.toString().length == 1) {
-             day = '0'+day;
-        }   
-        if(hour.toString().length == 1) {
-             hour = '0'+hour;
-        }
-        if(minute.toString().length == 1) {
-             minute = '0'+minute;
-        }
-        if(second.toString().length == 1) {
-             second = '0'+second;
-        }   
-        var dateTime = year+'/'+month+'/'+day+' '+hour+':'+minute+':'+second;   
-         return dateTime;
-}
-
-async function sendmail() {
-    let transporter = nodemailer.createTransport({
-        host: host,
-        port: port,
-        secure: false, 
-        auth: {
-            user: username, 
-            pass: password, 
-         },
-        tls: {rejectUnauthorized: false},
-        pool: true,
-    });
-    if (mattachment == null) {
-        let info = await transporter.sendMail({
-            from: {              
-                name: nsender,
-                address: ssender,
-            }, 
-            to: receiver,
-            subject: subject, 
-            text: mcontent, 
-            html: mcontent, 
-        });
-    } else {
-        let info = await transporter.sendMail({
-            from: {              
-                name: nsender,
-                address: ssender,
-            }, 
-            to: receiver,
-            subject: subject, 
-            text: mcontent, 
-            html: mcontent, 
-            attachments: {  
-                path: mattachment, 
-            }
-        });
-    }
-}
 
 module.exports = {
     name: 'mail',
     description: 'Send a spoofed email with discord!',
-	execute(message, args) {
+    execute(message, args) {
         message.reply("Please provide the recipients address.");
         log("-----------------------------------------------------------------------------------------");
-        log(currentTime + " " + message.author.tag +" has ran the mail command.");
+        log(currentTime() + " " + message.author.tag + " has ran the mail command.");
         let filter = (m) => !m.author.bot;
         const collector = new Discord.MessageCollector(
-        message.channel,
-        (m) => m.author.id === message.author.id,
-        { time: 1000000 }
+            message.channel,
+            (m) => m.author.id === message.author.id, { time: 1000000 }
         );
 
         var counter = 0;
@@ -108,26 +28,30 @@ module.exports = {
         collector.on("collect", (message) => {
             if (message.content && counter == 0) {
                 receiver = message.content;
-                log(currentTime + " " + message.author.tag +" - Mail Recipient Address: " + message.content);
+                log(currentTime() + " " + message.author.tag + " - Mail Recipient Address: " + message.content);
                 message.reply("Please provide a sender address.");
                 counter++;
-            } else if (message.content && counter == 1) {
+            }
+            else if (message.content && counter == 1) {
                 ssender = message.content;
-                log(currentTime + " " + message.author.tag +" - Mail Sender Address: " + message.content);
+                log(currentTime() + " " + message.author.tag + " - Mail Sender Address: " + message.content);
                 message.reply("Please provide a sender name.");
                 counter++;
-            } else if (message.content && counter == 2) {
+            }
+            else if (message.content && counter == 2) {
                 message.reply("Please provide a subject.");
                 nsender = message.content;
-                log(currentTime + " " + message.author.tag +" - Mail Sender Name: " + message.content);
+                log(currentTime() + " " + message.author.tag + " - Mail Sender Name: " + message.content);
                 counter++;
-            } else if (message.content && counter == 3) {
+            }
+            else if (message.content && counter == 3) {
                 message.reply("Please provide a message.");
                 subject = message.content;
-                log(currentTime + " " + message.author.tag +" - Mail Subject: " + message.content);
+                log(currentTime() + " " + message.author.tag + " - Mail Subject: " + message.content);
                 counter++;
-            } else if (message.content && counter == 4) {
-                log(currentTime + " " + message.author.tag +" - Mail Message: " + message.content);
+            }
+            else if (message.content && counter == 4) {
+                log(currentTime() + " " + message.author.tag + " - Mail Message: " + message.content);
 
                 var EventEmitter = require("events").EventEmitter;
                 var body = new EventEmitter();
@@ -135,11 +59,11 @@ module.exports = {
                 if (message.content.includes("http")) {
                     request.get(message.content, function(error, response, data) {
                         if (!error && response.statusCode == 200) {
-                        body.data = data;
-                        body.emit('update');
+                            body.data = data;
+                            body.emit('update');
                         }
                     });
-                    body.on('update', function () {
+                    body.on('update', function() {
                         mcontent = body.data
                     });
                 }
@@ -149,73 +73,79 @@ module.exports = {
 
                 message.reply('Would you like to add an attachment? (yes or no)')
                 counter++;
-                
-            } else if (message.attachments.first() && counter == 4) {
-				var EventEmitter = require("events").EventEmitter;
+
+            }
+            else if (message.attachments.first() && counter == 4) {
+                var EventEmitter = require("events").EventEmitter;
                 var body = new EventEmitter();
 
-				request.get(message.attachments.first().url, function(error, response, data) {
-					if (!error && response.statusCode == 200) {
-    				body.data = data;
-    				body.emit('update');
-    				}
+                request.get(message.attachments.first().url, function(error, response, data) {
+                    if (!error && response.statusCode == 200) {
+                        body.data = data;
+                        body.emit('update');
+                    }
                 });
-                
-                body.on('update', function () {
+
+                body.on('update', function() {
                     mcontent = body.data
                 });
-                
-                log(currentTime + " " + message.author.tag +" - Mail Message: " + message.attachments.first().url);
+
+                log(currentTime() + " " + message.author.tag + " - Mail Message: " + message.attachments.first().url);
 
                 message.reply("Would you like to add an attachment? (yes or no)")
                 counter++;
-              }
+            }
             else if (message.content && counter == 5) {
-                log(currentTime + " " + message.author.tag +" - Is there Attachment: " + message.content);
-            
+                log(currentTime() + " " + message.author.tag + " - Is there Attachment: " + message.content);
+
                 if (message.content == 'yes') {
                     message.reply("Please provide an attachment.")
                     counter++;
-                } else if (message.content == 'no') {
+                }
+                else if (message.content == 'no') {
                     message.reply("How many times would you like to send this email?")
                     counter++;
                     counter++;
-                } else {
+                }
+                else {
                     message.reply('Invalid')
                 }
-            } else if (message.attachments.first() && counter == 6) {
+            }
+            else if (message.attachments.first() && counter == 6) {
                 message.reply("How many times would you like to send this email?")
-                log(currentTime + " " + message.author.tag +" - Mail Attachment Url: " +message.attachments.first().url);
+                log(currentTime() + " " + message.author.tag + " - Mail Attachment Url: " + message.attachments.first().url);
 
                 mattachment = message.attachments.first().url
                 counter++;
-            } else if (message.content && counter == 6) {
+            }
+            else if (message.content && counter == 6) {
                 message.reply("How many times would you like to send this email?")
-                log(currentTime + " " + message.author.tag +" - Mail Attachment Url: " +message.content);
+                log(currentTime() + " " + message.author.tag + " - Mail Attachment Url: " + message.content);
                 mattachment = message.content
                 counter++;
-            } else if (counter == 7) {
-                log(currentTime + " " + message.author.tag +" - Is sending email " +message.content +" times ");
+            }
+            else if (counter == 7) {
+                log(currentTime() + " " + message.author.tag + " - Is sending email " + message.content + " times ");
                 unumberT = message.content
                 numberT = 0
                 while (numberT < unumberT) {
-                    sendmail(mattachment)
+                    sendmail(mattachment, nsender, ssender, receiver, subject, mcontent);
                     numberT++;
-                } if (numberT == unumberT) {
+                }
+                if (numberT == unumberT) {
                     message.reply("Email(s) Sent!");
 
-                    log("Sent Email With Nodemail!");
+                    log(currentTime() + " " + message.author.tag + " - Sent Email");
                     collector.stop();
-                    log("Collecter Ended.");
-                } if (unumberT == 0) {
-                    message.reply('No email sent!') 
+                    log(currentTime() + "Collector for - " + message.author.tag + " Ended!");
+                }
+                if (unumberT == 0) {
+                    message.reply('No email sent!')
                     collector.stop();
-                    log("User Cancelled!");
-                    log("Collecter Ended.");
-                } 
-            } 
+                    log(currentTime() + " " + message.author.tag + " Cancelled Email!")
+                    log(currentTime() + " Collector for - " + message.author.tag + " Ended!");
+                }
+            }
         });
-	},
+    },
 }
-
-
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const fs = require("fs");
 const Discord = require("discord.js");
-const {
-    prefix,
-    token
-} = require("./config.json");
+const { prefix, token } = require("./config.json");
 
+const currentTime = require("./events/time.js");
+const log = require("./events/log.js");
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -19,14 +18,14 @@ for (const file of commandFiles) {
 }
 
 client.once("ready", () => {
-    console.log(`${client.user.username} Ready!`);
+    log(currentTime() + " " + `${client.user.tag} online!`)
 });
 
-client.on("message", async (message) => {
+client.on("message", async(message) => {
     const args = message.content.slice(prefix.length).split(/ +/);
     const commandName = args.shift().toLowerCase();
     const command = client.commands.get(commandName);
-    const randPassword = Array(10).fill("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz").map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');
+    const randPassword = Array(10).fill("!@$%^&*0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz").map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');
 
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
@@ -35,16 +34,28 @@ client.on("message", async (message) => {
     try {
         if (commandName == "passgen") {
             message.channel.send(randPassword)
-        } else if (commandName == "ping") {
+            log(currentTime() + " " + message.author.tag + " generated a password")
+
+        }
+        else if (commandName == "ping") {
             const m = await message.channel.send("Pinging...");
             m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
-        } else {
+            log(currentTime() + " " + message.author.tag + " pinged the bot");
+        }
+        else {
             command.execute(message);
         }
-    } catch (error) {
+    }
+    catch (error) {
         console.error(error);
         message.reply('Something Went Wrong!');
     }
+});
+
+process.on("SIGINT", function() {
+    console.log("Goodbye!")
+    log("-----------------------------------------------------------------------------------------");
+    process.exit();
 });
 
 client.login(token);
