ea8ed5907073e4d8f4b3dc2d2791828a18f90817
ギャビンコラード <dev.greenpilot@gmail.com>
2020-11-09 01:16:40 +0000

    can send longer passwords via attachment

--- commands/passgen.js
+++ commands/passgen.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const { Client, MessageAttachment } = require('discord.js');
+
 const currentTime = require("../events/time.js");
 const log = require("../events/log.js");
 
@@ -8,7 +11,16 @@ module.exports = {
         const plength = args[0] | "10";
         const randPassword = Array(plength).fill("!@$%^&*0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz").map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');
 
-        message.channel.send(randPassword);
-        log(currentTime() + " " + message.author.tag + " Generated Password: " + randPassword);
+        if (plength > "1994") {
+            var buffer = Buffer.from(randPassword, "utf-8")
+            const attachment = new MessageAttachment(buffer, "pass.txt");
+            message.channel.send(attachment)
+            log(currentTime() + " " + message.author.tag + " Generated Password: " + "More then 2000 characters!");
+        }
+
+        else {
+            message.channel.send("```" + randPassword + "```");
+            log(currentTime() + " " + message.author.tag + " Generated Password: " + randPassword);
+        }
     },
 };
